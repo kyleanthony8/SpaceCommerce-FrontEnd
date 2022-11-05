3a7eee236da42f769c20415e7912c07d82495720
Danny Yu <tiramisu@Dannys-MBP.home>
2022-11-04 23:29:01 -0400

    initial profile frame

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from "react";
 import NavBar from './components/NavBar/NavBar';
+import UserProfile from "./screens/userProfile/UserProfile.jsx"
 import { Routes, Route } from "react-router-dom";
 
 
@@ -9,7 +10,8 @@ function App() {
     <div className="App">
       <NavBar />  
       <Routes>
-      <Route exact path="/" element={<h1>Home</h1>} />
+        <Route exact path="/" element={<h1>Home</h1>} />
+        <Route path="account" element={<UserProfile />}></Route>
       <Route path=":any" element={<h1>Not Home</h1>}></Route>
     </Routes>
 
