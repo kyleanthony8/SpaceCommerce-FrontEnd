c358000ad7569cd3bb0a9c9353a25227adbcaa57
Kyle Anthony <kyle.anthony8@gmail.com>
2022-11-07 09:37:52 -0500

    home

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,60 +1,42 @@
-import React, { useState } from "react";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
-import Modal from "react-bootstrap/Modal";
-import {
-  MDBContainer,
-  MDBNavbar,
-  MDBNavbarBrand,
-  MDBNavbarToggler,
-  MDBNavbarNav,
-  MDBNavbarItem,
-  MDBNavbarLink,
-  MDBCollapse,
-} from "mdb-react-ui-kit";
+import React from 'react'
+import styles from "./style";
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import UserProfile from "./screens/userProfile/UserProfile.jsx";
+import SignUp from "./screens/SignUp/SignUp";
+import SignIn from  "./screens/SignIn/SignIn"
+import CreateList from "./screens/createList/CreateList";
+import { Routes, Route } from "react-router-dom";
+import SpaceItem from "./screens/allList/SpaceItem.jsx";
+import { Navbar,
+  Card,
+  Filter,
+  Footer,
+  Hero, Body } from './components'
 
-function Home() {
-  return (
-    <div>
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Space Ship</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Ship Listing</Button>
-        </Card.Body>
-      </Card>
 
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Planet</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Ship Listing</Button>
-        </Card.Body>
-      </Card>
-
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Space Craft</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Ship Listing</Button>
-        </Card.Body>
-      </Card>
-    </div>
-  );
-}
-
-export default Home;
+  const App = () => (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+  
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+      
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Filter />
+          <Body />
+          <p className='heading2' Body goes here />
+          <Footer />
+        </div>
+      </div>
+      </div>
+      );
+  export default App;
