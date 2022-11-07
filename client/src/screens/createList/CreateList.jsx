a4cf3ff95446cadf75b8340ce4d8cf85cffbf4cd
jscsonka <113357554+jscsonka@users.noreply.github.com>
2022-11-07 11:41:52 -0500

    Input Forms

--- client/src/screens/createList/CreateList.jsx
+++ client/src/screens/createList/CreateList.jsx
@@ -1,12 +1,50 @@
 import './CreateList.css';
+import React from 'react';
 
 function CreateList() {
   return (
     <div className='createList'>
       <div className='center'>
-        <p>Name</p>
-        <p>Size</p>
-        <p>Price</p>
+        <h1>Create Listing</h1>
+        <p>Listing Information</p>
+        
+      <form id="inputName">
+        <label>
+        Name:
+        <input type="text" name="name" />
+        </label>
+        </form>
+      <form id="inputType">
+        <label>
+        Type:
+        <input type="text" name="name" />
+        </label>
+        </form>
+      <form id="inputSize">
+        <label>
+        Size:
+        <input type="text" name="name" />
+        </label>
+        </form>
+      <form>
+        <label id="inputPrice">
+        Price:
+        <input type="text" name="name" />
+        </label>
+        </form>
+      <form id="inputLocation">
+        <label>
+        Location:
+        <input type="text" name="name" />
+        </label>
+        </form>
+      <form id="uploadImage">
+        <label>
+        Upload Image:
+        <input type="file" name="name" />
+        </label>
+        </form>
+     <button id="createListing">Create Listing</button>
       </div>
 
     </div>
