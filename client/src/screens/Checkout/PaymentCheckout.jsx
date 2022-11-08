4e98aa13bb210c3ef75d3657d0bbecc818a54643
Andrea Alonso <andreaalonso@andreas-air.myfiosgateway.com>
2022-11-07 21:41:50 -0500

    added coinbase and apple pay options

--- client/src/screens/Checkout/PaymentCheckout.jsx
+++ client/src/screens/Checkout/PaymentCheckout.jsx
@@ -4,8 +4,14 @@ import Input from "./Input";
 
 const PaymentCheckout = (props) => (
   <div className="checkout">
-     <div className="checkout-container">
-      <h3 className="heading-3">Credit card checkout</h3>
+    <div className="checkout-container">
+      <button className="apple-pay-btn" onClick={event => window.location.href="https://www.apple.com/apple-pay/"}>Apple Pay</button>
+      <br />
+      <br />
+      <button className="crypto-btn" onClick={event => window.location.href='https://www.coinbase.com/'}>Pay with crypto</button>
+      <br />
+      <br />
+      <h3 className="heading-3">Or pay with card</h3>
       <Input label="Cardholder's Name" type="text" name="name" />
       <Input label="Card Number" type="number" name="card_number" imgSrc="https://seeklogo.com/images/V/visa-logo-6F4057663D-seeklogo.com.png" />
        <div className="row">
--- client/src/screens/Checkout/checkout.jsx
+++ client/src/screens/Checkout/checkout.jsx
@@ -3,8 +3,19 @@ import "./styles.css";
 import Item from "./Item";
 import PaymentCheckout from "./PaymentCheckout";
 
-const Checkout = () => {
-  
+const Checkout = (props) => {
+  return (
+    <div className="app-container">
+      <div className="row">
+        <div className="col">
+          <Item name={props.name} price={props.price} img={props.img} />
+        </div>
+        <div className="col no-gutters">
+          <PaymentCheckout />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Checkout;
