3518c2d94e8e3f9a3c93a30964e4a2581c2e70d7
jscsonka <113357554+jscsonka@users.noreply.github.com>
2022-11-08 00:08:11 -0500

    Create Listing CSS

--- client/src/screens/createList/CreateList.jsx
+++ client/src/screens/createList/CreateList.jsx
@@ -1,48 +1,55 @@
 import './CreateList.css';
 import React from 'react';
+import '../createList/images/starburst.png'
+import logo from '../createList/images/logo.png'
 
 function CreateList() {
   return (
     <div className='createList'>
       <div className='center'>
+      <img src={logo}/>
         <h1>Create Listing</h1>
         <p>Input listing information below</p>
         
-      <form id="inputName">
+        <form className="inputName">
         <label>
         Name:
         <input type="text" name="name" placeholder=" Planet-X" />
         </label>
         </form>
-      <form id="inputType">
+      <form className="inputType">
         <label>
         Type:
         <input type="text" name="name" placeholder=" Gas Giant" />
         </label>
         </form>
-      <form id="inputSize">
+      <form className="inputSize">
         <label>
         Size:
         <input type="text" name="name" placeholder=" Large"/>
         </label>
         </form>
       <form>
-        <label id="inputPrice">
+        <label className="inputPrice">
         Price:
         <input type="text" name="name" placeholder=" 1,000,000" />
         </label>
         </form>
-      <form id="inputLocation">
+      <form className="inputLocation">
         <label>
         Location:
         <input type="text" name="name" placeholder=" Milky Way" />
         </label>
         </form>
-      <form id="uploadImage">
-        Upload Image:
+        <div classname="uploadImage">
+      <button className="uploadButton">
+          Upload Image:
+          </button>
+          </div>
+        <form classname="chooseFile">
         <input type="file" name="name" />
         </form>
-        <button id="createListing">Create Listing</button>
+        <button className="createButton">Save Listing</button>
         
       </div>
 
