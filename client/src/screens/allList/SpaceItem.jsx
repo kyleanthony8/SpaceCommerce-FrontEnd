c54398dcd2234db5dadc8745d12d3da23fce36a9
Connor Harris <connorharris@Connors-MacBook-Pro-2.local>
2022-11-08 11:20:24 -0500

    added basic all listing page

--- client/src/screens/allList/SpaceItem.jsx
+++ client/src/screens/allList/SpaceItem.jsx
@@ -1,23 +1,47 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import ItemDisplay from "../../components/ItemDisplay.jsx";
 import { getSpaceItems } from "../../services/spaceItem.js";
+import { useLocation } from "react-router-dom";
 
 const SpaceItem = (props) => {
   const [items, setItems] = useState([]);
+  const [number, setNumber] = useState([]);
+  const num = items.length;
+  const path = useLocation();
 
+  const spaceData = items.map((item) => {
+    const space = {};
+    space.name = item.name;
+    space.price = item.price;
+
+    item.image.length != 0
+      ? (space.image = item.image)
+      : (space.image = [{ name: "No Image", image: "" }]);
+
+    return space;
+  });
+
+  const changeNumber = () => {
+    if (path.pathname === "/") {
+      setNumber(12);
+    } else {
+      setNumber(num);
+    }
+  };
   useEffect(() => {
     const fetchItems = async () => {
       const allItems = await getSpaceItems();
       setItems(allItems);
+      changeNumber();
     };
     fetchItems();
   }, []);
-console.log(items)
+
   return (
     <div>
       {items.length > 0 ? (
         <div className="items">
-          {items.slice(0, 12).map((item, index) => {
+          {spaceData.slice(0, number).map((item, index) => {
             return <ItemDisplay item={item} key={index} />;
           })}
         </div>
