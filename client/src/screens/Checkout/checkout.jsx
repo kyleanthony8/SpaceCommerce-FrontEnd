8b461838a08cecdbbd8f758683dedef5f35d1e92
Andrea Alonso <andreaalonso@andreas-air.myfiosgateway.com>
2022-11-07 22:09:48 -0500

    adjusted item container and item img and product col and gutters

--- client/src/screens/Checkout/checkout.jsx
+++ client/src/screens/Checkout/checkout.jsx
@@ -6,13 +6,11 @@ import PaymentCheckout from "./PaymentCheckout";
 const Checkout = (props) => {
   return (
     <div className="app-container">
-      <div className="row">
-        <div className="col">
-          <Item name={props.name} price={props.price} img={props.img} />
-        </div>
-        <div className="col no-gutters">
-          <PaymentCheckout />
-        </div>
+      <div className="product-col">
+        <Item name="Instax Mini 90 Neo Classic" price="$144.99" img="http://ecx.images-amazon.com/images/I/61%2BABMMN5zL._SL1500_.jpg" />
+      </div>
+      <div className="col no-gutters">
+        <PaymentCheckout />
       </div>
     </div>
   );
