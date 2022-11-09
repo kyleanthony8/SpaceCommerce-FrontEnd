d7fdb43c2b0d5be79fbc6e67ee2fdf75fb342823
Andrea Alonso <andreaalonso@andreas-air.myfiosgateway.com>
2022-11-09 00:32:01 -0500

    singlelisting css styling complete

--- client/src/screens/Singlelisting/Singlelisting.jsx
+++ client/src/screens/Singlelisting/Singlelisting.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect, useMemo } from "react";
 import { useParams } from "react-router-dom";
 import { getSpaceItem } from "../../services/spaceItem.js";
+import Item from "../Checkout/Item";
+import "./Singlelisting.css";
 
 const Singlelisting = () => {
   const [item, setItem] = useState({});
@@ -17,14 +19,24 @@ const Singlelisting = () => {
   if (!Object.keys(item).length) return <h1>Loading...</h1>;
 
   return (
-    <div className="Display">
-      <img src={item.image[0].image} />
-      <h2>{item.name}</h2>
-      <h3>{item.price} BTC</h3>
-      <p>{item.location}</p>
-      <h1>TEST TEST</h1>
-      <button>Fav</button>
-      <button>Buy</button>
+    <div className="listing-container">
+      <div className="listing-image">
+        <div className="item-container">
+          <div className="item-image">
+            <img src={item.image[0].image}/>
+          </div>
+        </div>
+      </div>
+      <div className="listing">
+        <div className="listing-details">
+          <div className="listing-details-container">
+            <h2 className="listing-item-h2">{item.name}</h2>
+            <h3 className="listing-item-h3">{item.price} BTC</h3>
+            <p className="listing-item-p">{item.location}</p>
+            <button className="buy-btn">Buy</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
