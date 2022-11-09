b98e2b2da4242bbb6216b82e121f666cc90e4e35
Danny Yu <tiramisu@Dannys-MacBook-Pro.local>
2022-11-09 10:22:53 -0500

    delete user

--- client/src/screens/userProfile/UserProfile.jsx
+++ client/src/screens/userProfile/UserProfile.jsx
@@ -2,12 +2,20 @@ import "./UserProfile.css";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { getUser } from "../../services/user";
+import { deleteUser } from "../../services/user";
 import { useAuthContext } from "../../hooks/useAuthContext";
 
 function UserProfile() {
   const [payload, setPayload] = useState({});
-  let navigate = useNavigate();
+  const navigate = useNavigate();
   const { user } = useAuthContext();
+  const { dispatch } = useAuthContext();
+
+  const DeleteUser = async () => {
+    await deleteUser();
+    dispatch({ type: "LOGOUT" });
+    navigate("/", { replace: true });
+  };
 
   const fetchItem = async () => {
     const res = await getUser(user._id);
@@ -34,9 +42,9 @@ function UserProfile() {
           <h1 className="userName">{payload.username}</h1>
           <h1 className="bioHeader">Bio</h1>
           <p className="bioText">Lorem ipsum dolor sit amet consecteturet dolore magna aliqua. </p>
-          <p className="emailAddress">Email Address</p>
         </div>
         <button className="changePass">Change Password</button>
+        <button onClick={DeleteUser} className="changePass">Delete Profile</button>
       </div>
       <div className="myList">My List</div>
     </div>
