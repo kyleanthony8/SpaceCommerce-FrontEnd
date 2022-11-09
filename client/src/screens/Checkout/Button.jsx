d3afe91cbc007bc97e7a35113034e960f3c55075
Danny Yu <tiramisu@Dannys-MacBook-Pro.local>
2022-11-09 14:13:48 -0500

    debug

--- client/src/screens/Checkout/Button.jsx
+++ client/src/screens/Checkout/Button.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./styles.css";
 import { buySpaceItem } from "../../services/spaceItem";
+import { useNavigate } from "react-router-dom";
 
 const Button = ({text, id}) => {
-
+  const navigate = useNavigate();
   const buyItem = async () => {
     await buySpaceItem(id);
+    navigate(`/details/${id}`, { replace: true });
   }
   
   return (
