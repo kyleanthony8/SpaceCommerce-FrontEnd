2691b7330e2b6edff0632f56976e1e55d6f99a47
Andrea Alonso <andreaalonso@andreas-air.myfiosgateway.com>
2022-11-08 22:44:04 -0500

    updtaed checkut styling and finalized signup css

--- client/src/screens/SignUp/SignUp.jsx
+++ client/src/screens/SignUp/SignUp.jsx
@@ -85,6 +85,7 @@ const SignUp = () => {
             name="email"
             ref={emailRef}
           />
+          <br/>
           <button className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`} type="submit">Create Account</button>
         </div>
       </div>
