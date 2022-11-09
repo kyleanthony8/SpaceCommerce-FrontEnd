6b6daf2ef54f571e994740f965308f3c993234d3
Kyle Anthony <kyle.anthony8@gmail.com>
2022-11-09 13:08:25 -0500

    Testimonials Added

--- client/src/components/Homecards.jsx
+++ client/src/components/Homecards.jsx
@@ -5,32 +5,14 @@ import { NavLink, useNavigate } from "react-router-dom";
 import styles from "../style";
 
 const Homecards = () => {
-  
   return (
     <div>
       <SpaceItem />
       <NavLink to="/allListing">
-        <div className="cont">
-
-
-          <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
-    <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
-      <div className={`${styles.flexStart} flex-row`}>
-        <p className="">
-          
-        </p>
+      <div className={`ss:hidden ${styles.flexCenter}`}>
         
       </div>
-      
-      
         <span className="text-gradient">See More Listings</span>
-      
-    </div>
-  </div>
-        
-
-
-        </div>
       </NavLink>
     </div>
   );
