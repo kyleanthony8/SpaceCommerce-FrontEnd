e947e6a48c296a2cdf9f429c64f769b5b30fc475
Andrea Alonso <andreaalonso@andreas-air.myfiosgateway.com>
2022-11-09 01:25:10 -0500

    edited checkout css

--- client/src/screens/Checkout/Item.jsx
+++ client/src/screens/Checkout/Item.jsx
@@ -3,10 +3,10 @@ import "./styles.css"
 const Item = (props) => (
   <div className="item-container">
     <div className="item-image">
-      <img src={props.img}/>
+      <img className="item-image-src" src={props.img}/>
       <div className="item-details">
         <h3 className="item-name"> {props.name} </h3>
-        <h2 className="item-price"> {props.price} </h2>
+        <h2 className="item-price"> {props.price} BTC</h2>
       </div>
     </div>
   </div>
